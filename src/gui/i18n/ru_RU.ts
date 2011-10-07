bcde2a38d40c9aa76834bf92641c0dceabb1cc1f
Anton Golubkov <anton.golubkov@gmail.com>
2011-10-07 22:48:33 +0700

    Add menu items, changes in preview panel interface

--- src/gui/i18n/ru_RU.ts
+++ src/gui/i18n/ru_RU.ts
@@ -3,93 +3,153 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="main_form_ui.py" line="142"/>
+        <location filename="main_form_ui.py" line="192"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="143"/>
+        <location filename="main_form_ui.py" line="193"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="144"/>
+        <location filename="main_form_ui.py" line="194"/>
         <source>Edit</source>
         <translation>Правка</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="145"/>
+        <location filename="main_form_ui.py" line="195"/>
         <source>View</source>
         <translation>Вид</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="146"/>
+        <location filename="main_form_ui.py" line="196"/>
         <source>Help</source>
         <translation>Справка</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="147"/>
+        <location filename="main_form_ui.py" line="197"/>
         <source>Processing</source>
         <translation>Обработка</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="148"/>
+        <location filename="main_form_ui.py" line="198"/>
         <source>Processing blocks</source>
         <translation>Блоки обработки</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="149"/>
+        <location filename="main_form_ui.py" line="199"/>
         <source>Preview</source>
         <translation>Просмотр</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="151"/>
+        <location filename="main_form_ui.py" line="203"/>
         <source>TextLabel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="153"/>
+        <location filename="main_form_ui.py" line="205"/>
         <source>Open ...</source>
         <translation>Открыть ...</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="154"/>
+        <location filename="main_form_ui.py" line="206"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="155"/>
+        <location filename="main_form_ui.py" line="207"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="156"/>
+        <location filename="main_form_ui.py" line="208"/>
         <source>Start</source>
         <translation>Старт</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="157"/>
+        <location filename="main_form_ui.py" line="209"/>
         <source>Stop</source>
         <translation>Стоп</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="158"/>
+        <location filename="main_form_ui.py" line="210"/>
         <source>Help Contents</source>
         <translation>Содержание</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="159"/>
+        <location filename="main_form_ui.py" line="211"/>
         <source>About Garland</source>
         <translation>О программе Garlnd</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="160"/>
+        <location filename="main_form_ui.py" line="212"/>
         <source>Save As ...</source>
         <translation>Сохранить как ...</translation>
     </message>
     <message>
-        <location filename="main_form_ui.py" line="152"/>
+        <location filename="main_form_ui.py" line="204"/>
         <source>Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="202"/>
+        <source>Keep</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="213"/>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="214"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="215"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="216"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="217"/>
+        <source>Find...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="218"/>
+        <source>Arrange scheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="219"/>
+        <source>Zoom in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="220"/>
+        <source>Zoom out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="221"/>
+        <source>Text mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="222"/>
+        <source>Image mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_form_ui.py" line="223"/>
+        <source>Icon mode</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
