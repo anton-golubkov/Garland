1d65f62a5eedc8222ac83f6c85dd419402266541
Anton Golubkov <anton.golubkov@gmail.com>
2011-10-14 21:42:27 +0700

    Add function for getting icon for port type

---
+++ src/gui/geticon.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+""" Functions for receive icons for blocks and ports   
+
+
+"""
+
+from PySide import QtGui
+
+__type_icons = {"IPFImage3cType" : "icons/picture.png",
+                "IPFImage1cType" : "icons/picture_gray.png",
+                "IPFRGBType" : "icons/rgb.png",
+                "IPFStringType" : "icons/text.png",
+                "IPFIntType" : "icons/integer.png",
+                "IPFFloatType" : "icons/integer.png",
+                }
+
+
+def get_image_for_type(type_name):
+    global __type_icons
+    if type_name not in __type_icons:
+        return QtGui.QImage() 
+    image = QtGui.QImage()
+    image.load(__type_icons[type_name])
+    return image
+
