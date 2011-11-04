419f6e267ff46a0b2ffd04765ac3b606c0c0e23f
Anton Golubkov <anton.golubkov@gmail.com>
2011-11-04 18:24:23 +0700

    Fix block creation in respect to IPFGraph interface change

--- src/ipf/ipfgraphloader.py
+++ src/ipf/ipfgraphloader.py
@@ -40,7 +40,14 @@ def load(file):
         block_name = block_node.attrib["name"]
         block_class_node = list(block_node)[0]
         class_name = block_class_node.tag
-        block = ipfblock_classes[class_name]()
+        
+        # Test for unknown block
+        if class_name not in ipfblock_classes:
+            raise TypeError("Unknown block class '%s'" % class_name)
+        
+        ## !! TODO Add reading of row and column
+        graph.add_block(class_name, block_name)
+        block = graph.get_block(block_name)
         properties_node = block_class_node.find("Properties")
         
         # Add properties to block
@@ -67,13 +74,8 @@ def load(file):
             property.set_value(value)
             block.properties[property_name] = property
             
-        # Add ports to block
         # !!! In this version we don`t load ports information 
         # because ports created in block constructor and can`t be deleted or created
-        
-        # Add block to graph
-        graph.add_block(block_name, block) 
-        
         
     # Load connections
     connections_node = ipfgraph_node.find("Connections")
@@ -81,11 +83,11 @@ def load(file):
         con_output_node = connection_node.find("ConnectionOutput")
         oblock_name = con_output_node.attrib["block"]
         oblock_port = con_output_node.attrib["port"]
-        oport = graph.__blocks[oblock_name].output_ports[oblock_port]
+        oport = graph.get_block(oblock_name).output_ports[oblock_port]
         con_input_node = connection_node.find("ConnectionInput")
         iblock_name = con_input_node.attrib["block"]
         iblock_port = con_input_node.attrib["port"]
-        iport = graph.__blocks[iblock_name].input_ports[iblock_port]
+        iport = graph.get_block(iblock_name).input_ports[iblock_port]
         graph.add_connection(oport, iport)
         
     return graph
