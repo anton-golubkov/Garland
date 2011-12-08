7a482773f246c6f89e17c354d1067d4062ab21a8
Anton Golubkov <anton.golubkov@gmail.com>
2011-12-08 22:17:17 +0700

    Add copyright header to source files

--- src/gui/main_form_ui.py
+++ src/gui/main_form_ui.py
@@ -1,3 +1,13 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2011 Anton Golubkov.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# 
+# Contributors:
+#     Anton Golubkov - initial API and implementation
+#-------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'main_form.ui'
